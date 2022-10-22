34ce5bf3f8da3a83d8c86102feb720a9227e7be3
Ruham Leal <ruhamxlpro@hotmail.com>
2022-10-22 01:19:53 -0300

    feat: adding a PUT request and a GET request that filters by book id

--- src/app.js
+++ src/app.js
@@ -17,9 +17,24 @@ app.get('/livros', (req, res) => {
   res.status(200).json(livros);
 });
 
+app.get('/livros/:id', (req, res) => {
+  const index = findBooks(req.params.id);
+  res.json(livros[index]);
+});
+
 app.post('/livros', (req, res) => {
   livros.push(req.body);
   res.status(201).send('Livro cadastrado com sucesso');
 });
+
+app.put('/livros/:id', (req, res) => {
+  const index = findBooks(req.params.id);
+  livros[index].titulo = req.body.titulo;
+  res.json(livros);
+});
+
+function findBooks(id) {
+  return livros.findIndex((book) => book.id == id);
+};
 
 export default app;
